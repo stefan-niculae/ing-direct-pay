c514191e3da83035045aad68d9395f52951e20dc
vagrant <vagrant@precise32.(none)>
2015-11-20 09:31:31 +0000

    Installing java and nvm.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,10 @@ chmod -R go-rwx ~vagrant/.ssh
 apt-get update
 apt-get install -y git
 
+apt-get install -y openjdk-7-jdk
+apt-get install -y leiningen
+apt-get install -y gradle
+apt-get install -y maven
+
+sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | HOME=/home/vagrant sudo -u vagrant bash
+
